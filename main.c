b87cdc783bceeb1322908bcf067e8e1a417dd371
Rodrigo Cabral <rodrigocabral0529@gmail.com>
2025-06-19 20:12:02 -0400

    Maze save
    I'm only saving this version because it's working, and I want to make numerous adjustments.

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 
 #define M_HEIGHT 6
 #define M_WIDTH 6
-#define VERTICAL 60
-#define HORIZONTAL 60
+#define VERTICAL 30
+#define HORIZONTAL 50
 #define HEIGHT (2*M_HEIGHT)+1
 #define WIDTH (2*M_WIDTH)+1
 
@@ -20,9 +20,9 @@ void mazeGen();
 int main()
 {
     srand(time(NULL));
-    //init();
-    //show();
+    init();
     mazeGen();
+    show();
     return 0;
 }
 
@@ -54,27 +54,33 @@ void show(){
     refresh();
 }
 void mazeGen(){
+    int number = 1;
     int mazeMatrix[M_HEIGHT+1][M_WIDTH];
+    
     for (size_t i = 0; i < M_HEIGHT; i++)
     {
         for (size_t j = 0; j < M_WIDTH; j++)
         {
-            mazeMatrix[i][j] = 0;
+            mazeMatrix[i][j] = 0;                 //Limpando Matriz antes de começar
         }
     }
     for (size_t i = 0; i < M_HEIGHT; i++)
     {
+        int arr[M_WIDTH*M_WIDTH] = {0};
         for (size_t j = 0; j < M_WIDTH; j++)
-        {
-            mazeMatrix[i][j] = j+1;
+        { 
+            if (mazeMatrix[i][j] == 0)
+            {
+                mazeMatrix[i][j] = number++;      //Adicionando Números
+            }
         }
-        for (size_t j = 1; j < M_WIDTH; j++)
+        for (size_t j = 1; j < M_WIDTH; j++)      //Randomizando conjuntos e juntando eles
         {
             int N = rand() % 100+1;
             if (N < HORIZONTAL && mazeMatrix[i][j] != mazeMatrix[i][j-1])
             {
                 int oldSet = mazeMatrix[i][j];
-                int newSet = mazeMatrix[i][j-1];
+                int newSet = mazeMatrix[i][j-1];     
 
                 for (size_t k = 0; k < M_WIDTH; k++)
                 {
@@ -83,18 +89,80 @@ void mazeGen(){
                         mazeMatrix[i][k] = newSet;
                     }
                 }
+            
+            }
+            for (size_t k = 1; k < M_WIDTH; k++)  //Unindo horizontalmente
+            {
+                if (mazeMatrix[i][k] == mazeMatrix[i][k-1])
+                {
+                    maze[i*2+1][k*2] = ' ';
+                }
+                
             }
         }
         
-    }
-    for (size_t i = 0; i < M_HEIGHT; i++)
-    {
-        for (size_t j = 0; j < M_WIDTH; j++)
-        {
-            printf("%d ",mazeMatrix[i][j]);
+        for (int j = 0; j < M_WIDTH; j++) {
+            arr[mazeMatrix[i][j]]++;              //Guardando aparições de números
         }
-        printf("\n");
+        for (int j = 0; j < M_WIDTH * M_WIDTH; j++) {
+            //printf("%d ",arr[j]);
+        }
+        //printf("\n\n");
+        for (int j = 0; j < M_WIDTH * M_WIDTH; j++) {       //Verificando número de aparições de conjuntos
+            int y = arr[j];
+            if (y == 0)                           
+            {
+                continue;
+            }else if(y == 1)
+            {
+                for (size_t k = 0; k < M_WIDTH; k++)
+                {
+                    if(mazeMatrix[i][k] == j){
+                        mazeMatrix[i+1][k] = j;
+                        if (i!=(M_HEIGHT-1))
+                        {
+                            maze[(i*2)+2][(k*2)+1] = ' ';
+                        }
+                        //printf("%d ",mazeMatrix[i][k]);
+                    }
+                    
+                }
+                
+            }else if(y > 1){
+                int k;
+                for (k = 0; k < M_WIDTH; k++)
+                {
+                    if(mazeMatrix[i][k] == j){
+                        break;
+                    }
+                };
+                if(mazeMatrix[i][k] == j){
+                    int M = rand() % arr[j];
+                    mazeMatrix[i+1][k+M] = j;
+                    if (i!=(M_HEIGHT-1))
+                    {
+                        maze[(i*2)+2][((k+M)*2)+1] = ' ';
+                    }
+                    //printf("%d ",mazeMatrix[i][k]);
+                }
+            }
+        }
+        //printf("\n\n");
     }
+    FILE *arquivo = fopen("labirinto.txt", "w");            //Salvando arquivo para fácil leitura
+    if (arquivo == NULL) {
+        printf("Erro ao criar o arquivo.\n");
+        return;
+    }
+
+    for (int i = 0; i < M_HEIGHT; i++) {
+        for (int j = 0; j < M_WIDTH; j++) {
+            fprintf(arquivo, "%d ", mazeMatrix[i][j]);
+        }
+        fprintf(arquivo, "\n");
+    }
+
+    fclose(arquivo);
 }
 
 void init(){
